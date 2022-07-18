83f9afb410bdda3189c7a26ce57486d823c6830f
Jens Gerke <jens.gerke@direktiv.io>
2022-07-18 10:36:09 +0200

    v1 pt2
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- apps/image-magick/v1.0.0/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t image-magick . && docker run -p 8080:8080 image-magick
--- apps/network/http/v1.0.0/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t http-request . && docker run -p 8080:8080 http-request
